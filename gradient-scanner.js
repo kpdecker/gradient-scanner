af0dfaa00d79e4e485e190e986c1a655e1f8440c
kpdecker <kpdecker@gmail.com>
2011-02-13 15:29:43 -0600

    Move line selector logic into a separate module.

--- gradient-scanner.js
+++ gradient-scanner.js
@@ -2,15 +2,13 @@
  * Copyright (c) 2011 Kevin Decker (http://www.incaseofstairs.com/)
  * See LICENSE for license information
  */
-var edgeContext;        // Global for now, move to ready scope once the file loader is complete
+/*global $, ColorStops, LineUtils */
+
+// Global namespace
+var GradientScanner = {};
+
 $(document).ready(function() {
-    const SNAP_TO_PX = 10;
-
-    var canvas = $("#imageDisplay"),
-        canvasOffset = canvas.offset(),
-        context = canvas[0].getContext("2d"),
-
-        linePreview = $(".line-preview"),
+    var linePreview = $(".line-preview"),
         colorStopsEl = $("#colorStops");
 
     // Provide a 1px transparent image for the preview images
@@ -23,17 +21,17 @@ $(document).ready(function() {
             + "${colorCss} ${position}"
         + "</div>");
 
-    var dragging, dragStart, dragEnd, relStart, relEnd, imageData, gradientType = "linear", colorStops, deltaE = ColorStops.JND;
+    var line, gradientType = "linear", colorStops, deltaE = ColorStops.JND;
 
     function outputGradient() {
-        var css = ColorStops.generateCSS(gradientType, dragStart, dragEnd, colorStops);
+        var css = ColorStops.generateCSS(gradientType, line.start, line.end, colorStops);
 
         ColorStops.applyBackground($("#gradientPreview"), "linear", {x: 0, y: 0}, {x: "100%", y: 0}, colorStops);
         $("#generatedCss")[0].textContent = "background-image: " + css.join(";\nbackground-image: ");
         $("#stopCount").text("Count: " + colorStops.filter(function(stop) { return !stop.disabled; }).length + " deltaE: " + deltaE);
     }
     function updateGradient() {
-        colorStops = ColorStops.extractColorStops(imageData.data, deltaE);
+        colorStops = ColorStops.extractColorStops(line.imageData.data, deltaE);
 
         colorStops.forEach(function(stop, index) {
             var stopEl = $.tmpl("colorStopTemplate", {
@@ -48,7 +46,12 @@ $(document).ready(function() {
     }
 
     function updatePreview() {
-        var containing = LineUtils.containingRect(dragStart, dragEnd, 25);
+        var containing = LineUtils.containingRect(line.start, line.end, 25),
+            relContaining = LineUtils.containingRect(line.start, line.end),
+
+            // Clip the coords to their containing boxes.
+            relStart = LineUtils.relativeCoords(line.start, relContaining),
+            relEnd = LineUtils.relativeCoords(line.end, relContaining);
 
         $(".preview-cell").css("left", containing.x+"px")
                 .css("top", containing.y+"px")
@@ -71,68 +74,8 @@ $(document).ready(function() {
         }
     });
 
-    canvas.parent().mousedown(function(event) {
-        // Only activate this if the event is due to a left click
-        if (event.which !== 1) {
-            return;
-        }
-
-        dragging = true;
-        var canvasOffset = canvas.offset();
-        dragStart = {x: event.pageX-canvasOffset.left, y: event.pageY-canvasOffset.top};
-
-        var edgeSnaps = ImageDataUtils.getInitialSnapToTarget(edgeContext, dragStart);
-        dragStart = edgeSnaps || dragStart;
-        // Init the line overlay
-        $("#lineOverlay").css("width", "0px")
-                .css("left", dragStart.x+"px")
-                .css("top", dragStart.y+"px");
-
-        colorStopsEl.html("");
-
-        event.preventDefault();
-    }).mousemove(function(event) {
-        if (dragging) {
-            var canvasOffset = canvas.offset();
-            dragEnd = {x: event.pageX-canvasOffset.left, y: event.pageY-canvasOffset.top};
-
-            // Check for snapto
-            if (Math.abs(dragEnd.y-dragStart.y) < SNAP_TO_PX) {
-                dragEnd.y = dragStart.y;
-            } else if (Math.abs(dragEnd.x-dragStart.x) < SNAP_TO_PX) {
-                dragEnd.x = dragStart.x;
-            }
-
-            // Check for edge snapto
-            var edgeSnap = ImageDataUtils.getSnapToTarget(edgeContext, dragStart, dragEnd);
-            dragEnd = edgeSnap || dragEnd;
-
-            // Collect the line data while the user is dragging
-            imageData = ImageDataUtils.getLinePixels(context, dragStart, dragEnd);
-            if (!imageData) {
-                return;
-            }
-
-            // Display the line preview
-            var stretcher = ImageDataUtils.createCanvasFromImageData(imageData);
-            linePreview.attr("src", stretcher.toDataURL());
-
-            // Move the line indicator
-            var distance = LineUtils.distance(dragStart, dragEnd),
-                rotate = "rotate(" + LineUtils.slopeInRads(dragStart, dragEnd) + "rad)";
-            $("#lineOverlay").css("width", distance)
-                    .css("-moz-transform", rotate)
-                    .css("-o-transform", rotate)
-                    .css("-webkit-transform", rotate);
-        }
-    }).mouseup(function(event) {
-        var containing = LineUtils.containingRect(dragStart, dragEnd);
-
-        // Clip the coords to their containing boxes.
-        relStart = LineUtils.relativeCoords(dragStart, containing);
-        relEnd = LineUtils.relativeCoords(dragEnd, containing);
-
-        dragging = false;
+    $(document).bind("lineUpdated", function(event) {
+        line = GradientScanner.line;
 
         updateGradient();
         updatePreview();
---
+++ line-selector.js
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2011 Kevin Decker (http://www.incaseofstairs.com/)
+ * See LICENSE for license information
+ */
+/*global $,jQuery,LineUtils,ImageDataUtils,GradientScanner */
+var edgeContext;        // Global for now, move to ready scope once the file loader is complete
+$(document).ready(function() {
+    const SNAP_TO_PX = 10;
+
+    var canvas = $("#imageDisplay"),
+        context = canvas[0].getContext("2d"),
+
+        linePreview = $(".line-preview");
+
+    var dragging, dragStart, dragEnd, imageData;
+
+    canvas.parent().mousedown(function(event) {
+        // Only activate this if the event is due to a left click
+        if (event.which !== 1) {
+            return;
+        }
+
+        dragging = true;
+        var canvasOffset = canvas.offset();
+        dragStart = {x: event.pageX-canvasOffset.left, y: event.pageY-canvasOffset.top};
+
+        var edgeSnaps = ImageDataUtils.getInitialSnapToTarget(edgeContext, dragStart);
+        dragStart = edgeSnaps || dragStart;
+        // Init the line overlay
+        $("#lineOverlay").css("width", "0px")
+                .css("left", dragStart.x+"px")
+                .css("top", dragStart.y+"px");
+
+        event.preventDefault();
+    }).mousemove(function(event) {
+        if (dragging) {
+            var canvasOffset = canvas.offset();
+            dragEnd = {x: event.pageX-canvasOffset.left, y: event.pageY-canvasOffset.top};
+
+            // Check for snapto
+            if (Math.abs(dragEnd.y-dragStart.y) < SNAP_TO_PX) {
+                dragEnd.y = dragStart.y;
+            } else if (Math.abs(dragEnd.x-dragStart.x) < SNAP_TO_PX) {
+                dragEnd.x = dragStart.x;
+            }
+
+            // Check for edge snapto
+            var edgeSnap = ImageDataUtils.getSnapToTarget(edgeContext, dragStart, dragEnd);
+            dragEnd = edgeSnap || dragEnd;
+
+            // Collect the line data while the user is dragging
+            imageData = ImageDataUtils.getLinePixels(context, dragStart, dragEnd);
+            if (!imageData) {
+                return;
+            }
+
+            // Display the line preview
+            var stretcher = ImageDataUtils.createCanvasFromImageData(imageData);
+            linePreview.attr("src", stretcher.toDataURL());
+
+            // Move the line indicator
+            var distance = LineUtils.distance(dragStart, dragEnd),
+                rotate = "rotate(" + LineUtils.slopeInRads(dragStart, dragEnd) + "rad)";
+            $("#lineOverlay").css("width", distance)
+                    .css("-moz-transform", rotate)
+                    .css("-o-transform", rotate)
+                    .css("-webkit-transform", rotate);
+        }
+    }).mouseup(function(event) {
+        dragging = false;
+
+        GradientScanner.line = {
+            start: dragStart,
+            end: dragEnd,
+            imageData: imageData
+        };
+
+        $(document).trigger(new jQuery.Event("lineUpdated"));
+    });
+});
